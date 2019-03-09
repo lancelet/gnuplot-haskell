16fbf6632fd95505a2b6ef84c31061ed2c358d16
Jonathan Merritt <j.s.merritt@gmail.com>
2019-03-09 18:41:31 +1100

    Commit  gnuplot-0.5.6

---
+++ execute/shell/Graphics/Gnuplot/Execute.hs
@@ -0,0 +1,26 @@
+module Graphics.Gnuplot.Execute where
+
+import Graphics.Gnuplot.Private.OS (gnuplotName, )
+import Graphics.Gnuplot.Utility (quote, semiColonConcat, )
+
+import System.Exit (ExitCode, )
+import System.Cmd (system, )
+
+
+simple ::
+      [String] {-^ The lines of the gnuplot script to be piped into gnuplot -}
+   -> [String] {-^ Options for gnuplot -}
+   -> IO ExitCode
+simple program options =
+   let cmd =
+          "sh -c 'echo " ++ quote (semiColonConcat program) ++
+                 " | " ++ gnuplotName ++ " " ++ unwords options ++ "'"
+   in  do --putStrLn cmd
+          system cmd
+
+{-
+escape :: String -> String
+escape ('\"':xs) = '\\' : '\"' : escape xs
+escape (x:xs)    = x : escape xs
+escape [] = []
+-}
